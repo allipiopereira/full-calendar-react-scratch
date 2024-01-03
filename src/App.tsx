099801beb2a424bb1b2be396f55b967ce4dbd16e
allipiopereira <allipiopereira@gmail.com>
2024-01-03 16:50:00 -0300

    init 🔥

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App;
